8e26a5a927b624ef50fb77cc19b3e5f59d396e00
maxanes <92018021+maxanes@users.noreply.github.com>
2023-12-27 14:15:04 +0100

    Update create_interval_and_pon.sh

--- create_interval_and_pon.sh
+++ create_interval_and_pon.sh
@@ -18,13 +18,13 @@ source $CONDA_BASE/bin/activate gatk-purecn
 export PURECN=$(conda env list | grep "*" | rev | cut -f1 -d' ' | rev)/lib/R/library/PureCN/extdata
 
 #Paths to the downloaded
-OUTDIR="/home/mnesic/PureCN/test_pipeline_paper/PON_test/retest" #Path/to/Output_folder
-fasta_file="/home/mnesic/genomic-struct/Workspaces/mnesic/WEStest/PureCN/extrafiles/hg38.fa" #Path/to/downloaded/fasta_file
-map_file="/home/mnesic/genomic-struct/Workspaces/mnesic/WEStest/PureCN/extrafiles/GCA_000001405.15_GRCh38_no_alt_analysis_set_76.bw" #Path/to/downloaded/mapability_file
-capture_bed_file="/faststorage/project/genomic-struct/Workspaces/mnesic/WEStest/PureCN/extrafiles/exome_captures_improve/exome_v1_primary_targets.interval.bed" #Path/to/your/capture/bed_file
+OUTDIR="Path/to/Output_folder"
+fasta_file="Path/to/downloaded/fasta_file"
+map_file="Path/to/downloaded/mapability_file/GCA_000001405.15_GRCh38_no_alt_analysis_set_76.bw"
+capture_bed_file="Path/to/your/capture/bed_file" 
 
 #Create interval file
-##Recommendation for aech used kit create separate interval_file
+##Recommendation for each used kit create separate interval_file
 
 Rscript $PURECN/IntervalFile.R \
     --in-file $capture_bed_file \
@@ -37,7 +37,7 @@ Rscript $PURECN/IntervalFile.R \
 	
 	
 #Iterate through folders with bam files 
-for folder in /home/mnesic/WES_data/231001*/; do #Path/to/directorium with bam files
+for folder in Path/to/directory with indexed bam files; do 
   # Find the BAM file in the current folder
     bam_file=$(find "$folder" -type f -name "*_buffycoat.normal.alignment.bam")
   
@@ -57,7 +57,7 @@ done
 
 #Create pon
 ##Create pon for each kit used, do not mix different kits  
-ls -a /home/mnesic/PureCN/test_pipeline_paper/PON_test/retest/*_loess.txt.gz | cat > normal_coverages.list #Path/to/created/coverage_files/*
+ls -a Path/to/created/coverage_files/*_loess.txt.gz | cat > normal_coverages.list 
 
 Rscript $PURECN/NormalDB.R --out-dir "$OUTDIR" \
 --coverage-files normal_coverages.list \
